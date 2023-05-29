ad84308b134f1f60078a432f219b7d511a0052a8
pakitowka <whitesaint12@gmail.com>
2023-05-29 21:13:27 +0200

    Hotel class methods

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv", dtype={"id":str})
+
+
 class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+
     def book(self):
-        pass
+        """Book hotel by changing its availability to 'no'"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
-    def generate(self):
+    def __init__(self, customer_name, hotel_object):
         pass
+
+    def generate(self):
+        content = f"Name of the customer hotel"
+        return content
+
+
+print(df)
+hotel_ID = input("Enter here the id of the hotel: ")
+hotel = Hotel(hotel_ID)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("The hotel is not free.")
